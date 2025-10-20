033f2e88537f778cbbacdd6fd9b736bb21f10ed1
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-10-20 17:37:12 -0300

    Organizando Arquivos no Repositório

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const App = () => {
   return (
     <div className="app-container">
       <div className="finance-card">
-        <h1 className="title">💰 Sistema de Controle Financeiro com Sheets API wwww</h1>
+        <h1 className="title">💰 TESTE Sistema de Controle Financeiro com Sheets API wwww</h1>
 
         <form onSubmit={handleSubmit} className="finance-form">
           <div className="form-group">
